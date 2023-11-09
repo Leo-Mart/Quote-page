5d7a19f13e50c025e178850f8d42b737000d7b53
Martin Leo <martin.christoffer.leo@gmail.com>
2023-11-09 17:05:54 +0100

    started with the basic functionallity, getting the next quote, getting a random quote and clearing the quote. A bunch of stuff has bugs in it or doesn't work the way I want it to, but that gives me something to work towards I guess.

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# A little practice project solely for myself. 
+# A little practice project mainly for myself. 
 The Idea is to have an array of quotes, without any specific theme or similar, instead merely quotes that I like be it poetry, prose, litle parables or whatever. 
 When the user clicks the button one of these quotes will show on the page. A few possible solutions for this, either randomized, or just looping through in order and then going back to the begining when the user reaches the end. 
-Could make it so that it shows a new quote everyday or something along those lines as well. 
+Could make it so that it shows a new quote everyday or something along those lines as well. ANd add a countdown to the next day for instance
+
+A further development could be to import quotes from a text file so I dont have to manually add stuff into the array everytime but instead do it dynamically somehow. 
+Could also import from a API somewhere with fetch.
+
+Maybe let the user display all the quotes.
+Or a tag system to display all the quotes by a certain author or with a specific theme or similar
+Add it all to a database and possibly let users add in their own quotes that will then show up on the page.
 
 Pretty simple project overall, just to practice a few things with javascript:
 *DOM manipulation
 *arrays
 *loops
 
-And while I build it try to remeber to document and reflect over what I've learned and found easy/hard or whatever.
+And while I build it try to remember to document and reflect over what I've learned and found easy/hard or whatever.
--- main.js
+++ main.js
@@ -1,6 +1,108 @@
+// array of objects to hold the quotes. For now I only have author and the actual quote as properties
+// could add say a source of the quote and year as well, but let's start simply
 quotes = [
-    {
-        author: "Oscar Wilde",
-        quote:  "The truth is rarely pure and never simple."
+  {
+    author: "Oscar Wilde",
+    quote: "The truth is rarely pure and never simple.",
+  },
+  {
+    author: "Oscar Wilde",
+    quote: "I am not young enough to know everything.",
+  },
+  {
+    author: "Oscar Wilde",
+    quote: "Experience is merely the name men gave to their mistakes.",
+  },
+  {
+    author: "Delamain (although technically the writers of Cyberpunk 2077)",
+    quote:
+      "When you live among humans, it's essential to find a shared, fundamental point of understanding. Truth and Good are values proven to cause division, whereas beauty is universal.",
+  },
+  {
+    author: "Shakespeare, spoken by Macbeth",
+    quote: `Tomorrow, and tomorrow, and tomorrow,
+      Creeps in this petty pace from day to day,
+      To the last syllable of recorded time;
+      And all our yesterdays have lighted fools
+      The way to dusty death. Out, out, brief candle!
+      Life\'s but a walking shadow, a poor player,
+      That struts and frets his hour upon the stage,
+      And then is heard no more. It is a tale
+      Told by an idiot, full of sound and fury,
+      Signifying nothing.`,
+  },
+  {
+    author: "David Foster Wallace",
+    quote: `None of this is about morality, or religion, or dogma, or big fancy questions of life after death. the capital-T Truth is about life before death.
+      It is about making it to thirty, or maybe even fifty, without wanting to shoot yourself in the head.
+      It is about the real value of a real education, which has nothing to do with grades or degrees and everything to do with simple awareness -- awareness of what is so real and essential, so hidden in plain sight all around us, that we have to keep reminding ourselves over and over:
+      This is Water.
+      This is Water.`,
+  },
+];
+
+//fecthing some stuff from the dom for manipulation
+let authorName = document.querySelector(".author");
+let quoteText = document.querySelector(".quote");
+let clearBtn = document.querySelector(".clear-quote");
+let getQuoteBtn = document.querySelector(".get-quote");
+let getRandomQuoteBtn = document.querySelector(".random-quote");
+// this counter for the function has to be in the global scope instead of inside the function, but i'm not sure why?
+let index = 0;
+
+quoteText.innerText = quotes[0].quote;
+authorName.innerText = quotes[0].author;
+
+// clicking the clear button to clear the quote. Not sure there is any value to this, but why not
+clearBtn.addEventListener("click", clearQuote);
+
+//click the get new quote button to load in a quote
+getQuoteBtn.addEventListener("click", getNextQuote);
+
+// click this button to randomize the quote that is presented. This works well enough but sometimes I can return the same value twice, so got to figure out a way for it to not do that. Probably an if statement of some kind
+getRandomQuoteBtn.addEventListener("click", function () {
+  random = Math.floor(Math.random() * quotes.length);
+  quoteText.innerText = quotes[random].quote;
+  authorName.innerText = quotes[random].author;
+});
+
+// functions
+
+// clear the quote
+function clearQuote() {
+  quoteText.innerText = "";
+  authorName.innerText = "";
+  index = 0;
+}
+
+// get next quote
+
+function getNextQuote() {
+  // had the let index = 0; in here at first, but that did not work, only increase it by one and then stayed on one
+  // maybe I have to pass the array of quotes and the counter as parameters/arguments?
+  index++;
+  console.log(index);
+  quoteText.innerText = quotes[index].quote;
+  authorName.innerText = quotes[index].author;
+  // trying to catch the error that happens when the counter goes beyond the length of the array, but not working
+  if (index > quotes.length) {
+    index = 0;
+  }
+}
+
+// probably don't need this function, but maybe it would be better to build a function for it, escpecially if I want to build in a way for it to not show the same quote twice in a row if it randoms out the same number
+//function getRandomQuote() {
+// If i remember random numbers in javascript right math.random gives me a random number between 0-1 so if i want to use it to randomize an index for an array I have to round it down the nearest full integer and then multiply with the current value of a counter/iteration "i"
+// random = Math.random();
+// console.log(random);
+//}
+
+/* function getNewQuoteLoop(){
+    for (i = 0; i < quotes.length; i++) {
+        author = quotes[i].author;
+        quote = quotes[i].quote;
+        console.log(author);
+        quoteText.textContent = quote;
+        authorName.textContent = author;
     }
-]
+} */
